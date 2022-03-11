d80db3be9da5a52c8f4f974be9d2990984604aa4
Alena_1706 <alena-antipina.antipina@yandex.ru>
2022-03-11 20:09:52 +0300

    solved

--- src/main/java/com/epam/autotasks/collections/MedianQueue.java
+++ src/main/java/com/epam/autotasks/collections/MedianQueue.java
@@ -1,32 +1,99 @@
 package com.epam.autotasks.collections;
 
-import java.util.AbstractQueue;
-import java.util.Iterator;
+import java.util.*;
 
-class MedianQueue<E extends Comparable<E>> extends AbstractQueue<E> {
+class MedianQueue<Integer extends Comparable<Integer>> extends AbstractQueue<Integer> {
+
+    Queue<Integer> queue = new ArrayDeque<>();
+
+    public void QueueMedium(Queue<Integer> queue){
+
+        Queue<Integer> startQ = new PriorityQueue<>();
+        while (!queue.isEmpty()){
+            startQ.offer(queue.poll());
+        }
+//        System.out.println("\nstartQ:");
+//        startQ.forEach(System.out::println);
+
+        PriorityQueue<Integer> sortedQueue = new PriorityQueue<>();
+        while (!startQ.isEmpty()){
+            sortedQueue.offer(startQ.poll());
+        }
+//        System.out.println("\nsorted:");
+//        sortedQueue.forEach(System.out::println);
+
+        Deque<Integer> first = new ArrayDeque<>();
+        Queue<Integer> second = new PriorityQueue<>();
+        int next;
+        int size = sortedQueue.size();
+        if (size % 2 == 0){
+            for (int i = 0; i < size / 2; i++){
+                first.push(sortedQueue.poll());
+            }
+            next = size / 2;
+        }
+        else{
+            for (int i = 0; i < size / 2 + 1; i++){
+                first.push(sortedQueue.poll());
+            }
+            next = size / 2 + 1;
+        }
+
+        for (int i = next; i < size; i++){
+            second.offer(sortedQueue.poll());
+        }
+
+//        System.out.println("\nfirst:");
+//        first.forEach(System.out::println);
+//        System.out.println("\nsecond:");
+//        second.forEach(System.out::println);
+
+        if (size % 2 != 0){
+            queue.offer(first.poll());
+        }
+
+        while (!first.isEmpty() || !second.isEmpty()){
+            Integer elem1 = first.poll();
+            if (elem1 != null){
+                queue.offer(elem1);
+            }
+            Integer elem2 = second.poll();
+            if (elem2 != null){
+                queue.offer(elem2);
+            }
+        }
+//        System.out.println("\nqueue:");
+//        queue.forEach(System.out::println);
+
+    }
+
+
 
     @Override
-    public Iterator<E> iterator() {
-        throw new UnsupportedOperationException();
+    public Iterator<Integer> iterator() {
+        return queue.iterator();
     }
 
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return queue.size();
     }
 
     @Override
-    public boolean offer(final E e) {
-        throw new UnsupportedOperationException();
+    public boolean offer(final Integer e) {
+        return queue.offer(e);
     }
 
     @Override
-    public E poll() {
-        throw new UnsupportedOperationException();
+    public Integer poll() {
+        return queue.poll();
     }
 
     @Override
-    public E peek() {
-        throw new UnsupportedOperationException();
+    public Integer peek() {
+        QueueMedium(queue);
+        return queue.peek();
     }
+
 }
+
